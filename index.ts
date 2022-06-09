bc5693fca1dc156630347fe52e1f151c140139bb
Jose Alvarez <josedav_96@hotmail.com>
2022-06-09 10:22:46 -0500

    fix> removing DEV subfix of env variables

--- index.ts
+++ index.ts
@@ -7,9 +7,9 @@ const logger = require("pino")();
 config();
 
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN_DEV,
-  signingSecret: process.env.SLACK_SIGNING_SECRET_DEV,
-  appToken: process.env.SLACK_APP_TOKEN_DEV, // add this
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  appToken: process.env.SLACK_APP_TOKEN, // add this
   port: process.env.BOT_PORT as any,
   customRoutes: [
     {
